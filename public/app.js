7f080fb2feec35588fc6405207600c417a3b1658
InsufiKoder <realomrrkn@gmail.com>
2023-04-19 23:13:21 +0300

    modified:   public/app.js 	modified:   server.js 	modified:   views/index.ejs

--- public/app.js
+++ public/app.js
@@ -2,28 +2,34 @@ const form = document.getElementById("uploadForm");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
-  fetch("/upload", { method: "POST", body: formData })
-    .then((response) => response.json())
-    .then((fileInfo) => {
-      const table = document.querySelector("table tbody");
-      const row = document.createElement("tr");
-      const nameCell = document.createElement("td");
-      const nameLink = document.createElement("a");
-      nameLink.href = fileInfo.url;
-      nameLink.textContent = fileInfo.name;
-      nameCell.appendChild(nameLink);
-      const sizeCell = document.createElement("td");
-      sizeCell.textContent = `${fileInfo.size} bytes`;
-      const actionCell = document.createElement("td");
-      const downloadLink = document.createElement("a");
-      downloadLink.href = fileInfo.url;
-      downloadLink.setAttribute("download", "");
-      downloadLink.textContent = "Download";
-      actionCell.appendChild(downloadLink);
-      row.appendChild(nameCell);
-      row.appendChild(sizeCell);
-      row.appendChild(actionCell);
-      table.appendChild(row);
-    })
-    .catch((error) => console.error(error));
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/upload");
+  xhr.upload.addEventListener("progress", (event) => {
+    const progressBar = document.getElementById("progressBar");
+    const percent = Math.round((event.loaded / event.total) * 100);
+    progressBar.value = percent;
+  });
+  xhr.addEventListener("load", (event) => {
+    const fileInfo = JSON.parse(xhr.responseText);
+    const table = document.querySelector("table tbody");
+    const row = document.createElement("tr");
+    const nameCell = document.createElement("td");
+    const nameLink = document.createElement("a");
+    nameLink.href = fileInfo.url;
+    nameLink.textContent = fileInfo.name;
+    nameCell.appendChild(nameLink);
+    const sizeCell = document.createElement("td");
+    sizeCell.textContent = `${fileInfo.size} bytes`;
+    const actionCell = document.createElement("td");
+    const downloadLink = document.createElement("a");
+    downloadLink.href = fileInfo.url;
+    downloadLink.setAttribute("download", "");
+    downloadLink.textContent = "Download";
+    actionCell.appendChild(downloadLink);
+    row.appendChild(nameCell);
+    row.appendChild(sizeCell);
+    row.appendChild(actionCell);
+    table.appendChild(row);
+  });
+  xhr.send(formData);
 });
--- server.js
+++ server.js
@@ -4,43 +4,68 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-
-// Set up multer to store uploaded files with their original names and extensions
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const name = path.basename(file.originalname, ext);
-    cb(null, name + "-" + Date.now() + ext);
-  },
-});
-const upload = multer({ storage });
-
 app.use(express.static("public"));
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.post("/upload", upload.single("file"), (req, res) => {
-  const file = req.file;
-  const fileInfo = {
-    name: file.originalname,
-    size: file.size,
-    url: `/uploads/${file.filename}`,
-  };
-  res.json(fileInfo);
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, cb) =>
+      cb(
+        null,
+        `${path.parse(file.originalname).name}-${Date.now()}${path.extname(
+          file.originalname
+        )}`
+      ),
+  }),
+  fileFilter: (req, file, cb) => {
+    if (/\.exe$|\.bat$/i.test(file.originalname)) {
+      return cb(new Error("Executables are not allowed"));
+    }
+    cb(null, true);
+  },
+  limits: { fileSize: 250 * 1024 * 1024 },
+}).single("file");
+
+const checkFileRestrictions = (file) => {
+  if (/\.exe$|\.bat$/i.test(file.originalname)) {
+    return "Executables are not allowed";
+  }
+  if (file.size > 250 * 1024 * 1024) {
+    return "File size is too large";
+  }
+  return null;
+};
+
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      if (err instanceof multer.MulterError) {
+        if (err.code === "LIMIT_FILE_SIZE") {
+          return res.status(400).send("File too large");
+        }
+      }
+      return res.status(500).send("Internal server error");
+    }
+    const error = checkFileRestrictions(req.file);
+    if (error) {
+      fs.unlinkSync(req.file.path);
+      return res.status(400).send(error);
+    }
+    const fileInfo = {
+      name: req.file.originalname,
+      size: req.file.size,
+      url: `/uploads/${req.file.filename}`,
+    };
+    res.json(fileInfo);
+  });
 });
 
 app.get("/uploads/:filename", (req, res) => {
   const { filename } = req.params;
   const filePath = path.join(__dirname, "uploads", filename);
-  if (fs.existsSync(filePath)) {
-    res.sendFile(filePath);
-  } else {
-    res.sendStatus(404);
-  }
+  fs.existsSync(filePath) ? res.sendFile(filePath) : res.sendStatus(404);
 });
 
 app.get("/", (req, res) => {
@@ -49,27 +74,31 @@ app.get("/", (req, res) => {
     name: file,
     url: `/uploads/${file}`,
   }));
-  res.render("index", { fileList: fileList }); // Ensure fileList is passed to the template
+  res.render("index", { fileList }); // Ensure fileList is passed to the template
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.locals.formatBytes = function (bytes, decimals = 2) {
-  function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return "0 Bytes";
-
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-  }
+app.locals.formatBytes = (bytes, decimals = 2) => {
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 };
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+// Error handler middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).send("File too large");
+    } else if (/.exe$|.bat$/i.test(err.message)) {
+      return res.status(400).send("Executables are not allowed");
+    }
+    return res.status(500).send("Internal server error");
+  }
 });
+
+app.listen(3000, () => console.log("Server running on port 3000"));
